7ce0c1f788acfd5284acd57032aba57b589c932d
SohamMangale <sohammangale60@gmail.com>
2024-08-10 15:18:43 +0530

    Create Exp1B.cpp

---
+++ Exp1B.cpp
@@ -0,0 +1,18 @@
+// Soham 
+// 23070123084
+// entc B1
+// experiment 1B
+#include<iostream>
+using namespace std;
+int main() {
+    int num1,num2,num3,sum=0;
+    cout<<"Enter first number-";
+    cin>>num1;
+    cout<<"Enter second number-";
+    cin>>num2;
+    cout<<"Enter third number-";
+    cin>>num3;
+    sum=num1+num2+num3;
+    cout<<"Sum of given numbers="<<sum;
+    return 0;
+}
